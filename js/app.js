d2648f795a44b5817d2b3c2bdd69aed706d73735
drixene <edrikcabbar@gmail.com>
2024-03-15 07:01:31 +0800

    update

--- js/app.js
+++ js/app.js
@@ -8,20 +8,20 @@ let Obj1 ={
     title: "LIGHTFURY",
     desc: "The Light Fury is an engine of change in our story, representing the call of the wild, which is key to Toothless' destiny. Dean DeBlois. Director Dean DeBlois describes the Light Fury's role as a call to the wild for Toothless, who lacks many primal instincts regarding love.",
     btn:"Contact Me",
-    myImg: "../image/qqq.jpg"
+    myImg: "../image/x1.png"
 }
 
 let b = document.getElementById("sectionTwo")
 
 let Obj2={
     title2: "Project Completed",
-    img1: "../image/lu.jpg",
+    img1: "../image/x2.jpg",
     desc1: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, est ratione aut veritatis placeat earum voluptatum blanditiis officiis soluta sunt quod atque modi dolor quibusdam, at illum commodi aspernatur quos.",
     btn1: "LearnMore",
-    img2: "../image/lo.jpg",
+    img2: "../image/x3.jpg",
     desc2: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, est ratione aut veritatis placeat earum voluptatum blanditiis officiis soluta sunt quod atque modi dolor quibusdam, at illum commodi aspernatur quos.",
     btn2: "Learn More",
-    img3: "../image/li.jpg",
+    img3: "../image/x4.png",
     desc3: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, est ratione aut veritatis placeat earum voluptatum blanditiis officiis soluta sunt quod atque modi dolor quibusdam, at illum commodi aspernatur quos.",
     btn3: "Learn More"
 }
